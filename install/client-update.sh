2fbd87af282ac5749239e31204a877b235d0b910
Aaron Prisk <aaronprisk@gmail.com>
2016-12-16 14:33:21 -0500

    Added Client Updater

---
+++ install/client-update.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#Lagertha Client Update Tool
+#Version 1.0
+
+if [ $EUID != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+fi
+
+clear
+echo "***********************************"
+echo "*** LAGERTHA CLIENT UPDATE TOOL ***"
+echo "***          VERSION 1.0        ***"
+echo "***********************************"
+echo
+
+wget -q http://lagertha.westbranch.org/client/VERSION -O /tmp/LAGVERSION
+
+source /tmp/LAGVERSION
+echo "Server Version is:"
+echo $LAGVERSION
+echo "-------------------"
+source /usr/share/lagertha/install/VERSION
+echo "Current Version is:"
+echo $CURVERSION
+echo
+
+if [[ $CURVERSION < $LAGVERSION ]]; then
+	echo "* STOPPING LAGERTHA SERVICE"
+	sudo service lagertha stop
+	echo "* DOWNLOADING UPDATE"
+        wget -q http://lagertha.westbranch.org/client/client.tar.gz -O /tmp/client.tar.gz
+	echo "* EXTRACTING UPDATE"
+	tar -xf /tmp/client.tar.gz -C /usr/share/lagertha
+	echo "* SETTING FILE PERMISSIONS"
+	sudo chmod +x /usr/share/lagertha/*.py
+	sudo chmod +x /usr/share/lagertha/*.sh
+	sudo chmod +x /usr/share/lagertha/install/*.sh
+	echo "* UPDATE COMPLETE. RESTARTING LAGERTHA CLIENT."
+	sudo service lagertha restart
+	exit
+fi
+
+echo "No update needed. Exiting update tool."
+exit
