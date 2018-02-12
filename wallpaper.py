454e6f0a78ebee68a6a0bd57db4285190ad01d4a
Aaron Prisk <aaronprisk@gmail.com>
2018-02-12 12:04:08 -0500

    Multi monitor wallpaper fix

--- wallpaper.py
+++ wallpaper.py
@@ -12,9 +12,10 @@ def wallpaper(lag_server,pkgname):
 		urllib.urlretrieve("http://" + lag_server + "/" + pkgname, filename="/usr/share/lagertha/wall.jpg")
 		status = 1
 		os.system("xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/image-path --set /usr/share/lagertha/wall.jpg")
+		os.system("xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor1/image-path --set /usr/share/lagertha/wall.jpg")
 		return status
 	except:
-		print "Couldn't download new wallpaper!!!"
+		print "Error: Couldn't download new wallpaper from server"
 		status = 2
 		return status
 		
