7df7f5c85668d3412e81f4c227092afaaa242c9f
Aaron Prisk <aaronprisk@gmail.com>
2018-03-07 10:39:41 -0500

    Removed XFCE specific command

--- wallpaper.py
+++ wallpaper.py
@@ -11,8 +11,6 @@ def wallpaper(lag_server,pkgname):
 	try:
 		urllib.urlretrieve("http://" + lag_server + "/" + pkgname, filename="/usr/share/lagertha/wall.jpg")
 		status = 1
-		os.system("xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/image-path --set /usr/share/lagertha/wall.jpg")
-		os.system("xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor1/image-path --set /usr/share/lagertha/wall.jpg")
 		return status
 	except:
 		print "Error: Couldn't download new wallpaper from server"
